700e6e7c2e2671da43b700da065e5e51a59474e9
samrunyan5 <samjorzak@gmail.com>
2021-01-25 23:39:34 -0600

    Starting files/code ready to go

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div id='app'></div>
+    )
+}
+
+RenderDOM.render(
+    <App />,
+    document.getElementById('root')
+)
